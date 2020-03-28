a70dbd1dd623b195fd288e8fe975a1b6e2bce9cf
tsharon-byte <sharonova_t@inbox.ru>
2020-03-28 18:03:49 +0300

    Updates

---
+++ precache-manifest.e8343aab646705a0124def4e2eadf2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ada4942dfe84bb844e191a2dbc261c",
+    "url": "/react-router-example/index.html"
+  },
+  {
+    "revision": "c38fad399db355b931c5",
+    "url": "/react-router-example/static/css/main.0ca5cb7d.chunk.css"
+  },
+  {
+    "revision": "57c37fd2125ea9d8ae39",
+    "url": "/react-router-example/static/js/2.dd4903d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router-example/static/js/2.dd4903d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38fad399db355b931c5",
+    "url": "/react-router-example/static/js/main.eed39618.chunk.js"
+  },
+  {
+    "revision": "430d025eeff80e8f1299",
+    "url": "/react-router-example/static/js/runtime-main.f3c5a620.js"
+  }
+]);
